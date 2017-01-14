93d2a75305888740cf12109ec4159f433902ff8a
Деніс <denys@Mac-Denis.local>
2017-01-14 17:36:49 +0200

    add printf and itoa_base

--- exam/ft_itoa_base/ft_itoa_base.c
+++ exam/ft_itoa_base/ft_itoa_base.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void	nbr_to_str(int value, int base)
+void	nbr_to_str(unsigned int value, unsigned int base, char **str)
 {
 	char c;
 
 	if (value >= base)
 	{
-		nbr_to_str(value/base, base);
-		nbr_to_str(value%base, base);
+		nbr_to_str(value/base, base, str);
+		nbr_to_str(value%base, base, str);
 	}
 	else
 	{
@@ -16,23 +16,41 @@ void	nbr_to_str(int value, int base)
 			c = value + 'A' - 10;
 		else
 			c = value + '0';
-		printf("%c", c);
+		**str = c;
+		(*str)++;
 	}
+	**str = '\0';
 }
 
 char	*ft_itoa_base(int value, int base)
 {
 	char	*str;
-
-	str = (char*)malloc(sizeof(char) * 33);
-	nbr_to_str(value, base);
+	char	*s;
 	
-	return (str);
+	str = (char*)malloc(sizeof(char) * 33);
+	*str = 0;
+	s = str;
+	if (value < 0)
+	{
+		if (base == 10)
+		{
+			*str = '-';
+			str++;
+		}
+		value *= -1;	
+	}
+	if (base < 2 || base > 16)
+		return (s);
+	nbr_to_str((unsigned int)value, (unsigned int)base, &str);
+	return (s);
 }
 
-int		main(void)
-
+int		main(int argc, char **argv)
+{
+	if (argc != 3)
+		return (0);
 	//printf("%d", 4 * 8);
-	printf("\n%s", ft_itoa_base(255, 12));
+	printf("%s\n", ft_itoa_base(atoi(argv[1]), atoi(argv[2])));
+	
 	return (0);
 }
--- ft_printf/ft_printf.c
+++ ft_printf/ft_printf.c
@@ -1,13 +1,34 @@
-#include "../libft/libft.h"
 
-int		ft_printf(const char *restrict format, ...)
+#include <stdio.h>
+#include "ft_printf.h"
+
+int		help(int nbr, ...)
 {
-	
-	return (0);
+
+int		ft_printf(char *str, ...)
+{
+	int		nbr;
+	int		count;
+	va_list arg_ptr;
+
+	if (!str)
+		return (-1);
+	count = 0;
+	nbr = count_nbr(str);
+	va_start(arg_ptr, nbr);
+	while (count < nbr)
+	{
+		printf("%d\n", va_arg(arg_ptr, int));
+		count++;
+	}
+	va_end(arg_ptr);
+}
+	return (1);
 }
 
 int main(void)
 {
-	ft_printf("aaa %s \n %s\n", "first", "second");
+	ft_printf("my first printf:\a %d\n", 10, 10, 10, 10);
+	ft_putnbr(printf("\n   %denys  \n", 15));
 	return (0);
 }
