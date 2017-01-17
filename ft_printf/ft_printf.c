eb12d63d7c4837b828f2e3609b49485b6c14b647
Деніс <denys@Mac-Denis.local>
2017-01-17 23:04:53 +0200

    ft_printf.c

--- ft_printf/ft_printf.c
+++ ft_printf/ft_printf.c
@@ -2,19 +2,27 @@
 #include <stdio.h>
 #include "ft_printf.h"
 
-int		help(int nbr, ...)
+char	*count_nbr(char *str, int *len)
 {
+	*len = 0;
+	while (str[*len] != '\0')
+	{
+		if (str[*len] == '%' && check_type(&str[*len]) > 0)
+		(*len)++;
+	}
+}
 
 int		ft_printf(char *str, ...)
 {
 	int		nbr;
 	int		count;
+	char	**strings;
 	va_list arg_ptr;
 
 	if (!str)
 		return (-1);
 	count = 0;
-	nbr = count_nbr(str);
+	count_nbr(str, &nbr);
 	va_start(arg_ptr, nbr);
 	while (count < nbr)
 	{
@@ -22,13 +30,15 @@ int		ft_printf(char *str, ...)
 		count++;
 	}
 	va_end(arg_ptr);
-}
 	return (1);
 }
 
 int main(void)
 {
-	ft_printf("my first printf:\a %d\n", 10, 10, 10, 10);
-	ft_putnbr(printf("\n   %denys  \n", 15));
+	char *ch = "sdfg  hj\n";
+
+//	ft_printf("my first printf:\a %d\n", 10, 10, 10, 10);
+//	ft_putnbr(printf("\n   %denys  \n", 15));
+	printf("%++fs", ch);
 	return (0);
 }
