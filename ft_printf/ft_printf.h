8a9fe9fa9471b04d163e08905af0577f0a713ba4
Деніс <denys@Mac-Denis.local>
2017-01-17 23:29:04 +0200

    ft_printf.c

--- ft_printf/ft_printf.h
+++ ft_printf/ft_printf.h
@@ -1,7 +1,19 @@
 
 #ifndef FT_PRINTF_H
 # define FT_PRINTF_H
+
 # include "../libft/libft.h"
 # include <stdarg.h>
+
+typedef	struct		s_typ
+{
+	struct s_typ	*next;
+	void			*data;
+	char			flags;
+	int				width;
+	int				precision;
+	char			size;
+	char			type;
+
 
 #endif
